f096c8108557ddbb696f64167e8ce5473cc56ff7
gaureliano <gaabriel.aureliano@outlook.com>
2021-01-20 21:02:12 -0300

    Implementing calculator

---
+++ 02_RESTAPI_calculator/RESTAPI/RESTAPI/Controllers/CalculatorController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RESTAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstnumber}/{secondnumber}")]
+        public IActionResult Get(string firstnumber, string secondnumber)
+        {
+            if(IsNumeric(firstnumber) && IsNumeric(secondnumber))
+            {
+                var sum = ConvertToDecimal(firstnumber) + ConvertToDecimal(secondnumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid value");
+        }
+
+        private bool IsNumeric(string strnumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(
+                strnumber,
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out number);
+            return isNumber;
+
+        }
+
+        private decimal ConvertToDecimal(string strnumber)
+        {
+            decimal decimalValue;
+            if(decimal.TryParse(strnumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        
+    }
+}
