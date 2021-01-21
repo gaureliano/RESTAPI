257ae8b2c116f42e69afd4504d82ef69f8373f16
gaureliano <gaabriel.aureliano@outlook.com>
2021-01-20 21:55:32 -0300

    calculator com todas funcoes

--- 02_RESTAPI_calculator/RESTAPI/RESTAPI/Controllers/CalculatorController.cs
+++ 02_RESTAPI_calculator/RESTAPI/RESTAPI/Controllers/CalculatorController.cs
@@ -21,7 +21,7 @@ namespace RESTAPI.Controllers
         }
 
         [HttpGet("sum/{firstnumber}/{secondnumber}")]
-        public IActionResult Get(string firstnumber, string secondnumber)
+        public IActionResult Sum(string firstnumber, string secondnumber)
         {
             if(IsNumeric(firstnumber) && IsNumeric(secondnumber))
             {
@@ -30,6 +30,63 @@ namespace RESTAPI.Controllers
             }
             return BadRequest("Invalid value");
         }
+
+        [HttpGet("sub/{firstnumber}/{secondnumber}")]
+        public IActionResult Subtraction(string firstnumber, string secondnumber)
+        {
+            if (IsNumeric(firstnumber) && IsNumeric(secondnumber))
+            {
+                var sub = ConvertToDecimal(firstnumber) - ConvertToDecimal(secondnumber);
+                return Ok(sub.ToString());
+            }
+            return BadRequest("Invalid value");
+        }
+
+        [HttpGet("mult/{firstnumber}/{secondnumber}")]
+        public IActionResult Multiplication(string firstnumber, string secondnumber)
+        {
+            if (IsNumeric(firstnumber) && IsNumeric(secondnumber))
+            {
+                var mult = ConvertToDecimal(firstnumber) * ConvertToDecimal(secondnumber);
+                return Ok(mult.ToString());
+            }
+            return BadRequest("Invalid value");
+        }
+
+        [HttpGet("div/{firstnumber}/{secondnumber}")]
+        public IActionResult Division(string firstnumber, string secondnumber)
+        {
+            if (IsNumeric(firstnumber) && IsNumeric(secondnumber))
+            {
+                var div = ConvertToDecimal(firstnumber) / ConvertToDecimal(secondnumber);
+                return Ok(div.ToString());
+            }
+            return BadRequest("Invalid value");
+        }
+
+        [HttpGet("med/{firstnumber}/{secondnumber}")]
+        public IActionResult Media(string firstnumber, string secondnumber)
+        {
+            if (IsNumeric(firstnumber) && IsNumeric(secondnumber))
+            {
+                var med = (ConvertToDecimal(firstnumber) + ConvertToDecimal(secondnumber))/2;
+                return Ok(med.ToString());
+            }
+            return BadRequest("Invalid value");
+        }
+
+        [HttpGet("sqrt/{firstnumber}")]
+        public IActionResult SquareRoot(string firstnumber)
+        {
+            if (IsNumeric(firstnumber))
+            {
+                var sqrt = Math.Sqrt((double)ConvertToDecimal(firstnumber)) ;
+                return Ok(sqrt.ToString());
+            }
+            return BadRequest("Invalid value");
+        }
+
+
 
         private bool IsNumeric(string strnumber)
         {
