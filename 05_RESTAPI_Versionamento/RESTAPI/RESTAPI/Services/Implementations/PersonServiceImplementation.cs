bc06c793ea64a7f6bf7384102c520fcf44e363d2
gaureliano <gaabriel.aureliano@outlook.com>
2021-01-26 20:09:51 -0300

    vers

---
+++ 05_RESTAPI_Versionamento/RESTAPI/RESTAPI/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using RESTAPI.Controllers.Model;
+using RESTAPI.Model.Context;
+
+namespace RESTAPI.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private MySQLContext _context;
+        private long id;
+
+        public PersonServiceImplementation(MySQLContext context)
+        {
+            _context = context;
+        }
+
+        public Person Create(Person person)
+        {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault(p => p.ID.Equals(id));
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+        }
+
+        public List<Person> FindAll()
+        {
+            return _context.Persons.ToList();
+        }
+
+        public Person FindByID(long id)
+        {
+            return _context.Persons.SingleOrDefault(p => p.ID.Equals(id));
+        }
+
+        public Person Update(Person person)
+        {
+            if (!Exists(person.ID)) return new Person();
+
+            var result = _context.Persons.SingleOrDefault(p => p.ID.Equals(id));
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            return person;
+        }
+
+        private bool Exists(long id)
+        {
+            return _context.Persons.Any(p => p.ID.Equals(id));
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                ID = 1,
+                FirstName = "Person Name" + i,
+                LastName = "Person Last Name" + i ,
+                Address = "Some address" + i,
+                Gender = "Male"
+            };
+        }
+
+    }
+}
