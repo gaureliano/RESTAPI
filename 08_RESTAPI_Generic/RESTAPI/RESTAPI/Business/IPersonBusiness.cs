d817a508d76d5fb0f46cb0c1d42374cc6edc8d17
gaureliano <gaabriel.aureliano@outlook.com>
2021-02-08 21:33:55 -0300

    generic

---
+++ 08_RESTAPI_Generic/RESTAPI/RESTAPI/Business/IPersonBusiness.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RESTAPI.Controllers.Model;
+
+namespace RESTAPI.Business
+{
+    public interface IPersonBusiness
+    {
+        Person Create(Person person);
+
+        Person FindByID(long id);
+
+        List<Person> FindAll();
+
+        Person Update(Person person);
+
+        void Delete(long id);
+    }
+}
