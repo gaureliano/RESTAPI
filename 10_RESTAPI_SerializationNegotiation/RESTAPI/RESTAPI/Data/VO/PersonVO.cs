f284230a970feec242e7161bbdfbc57eb9604f5c
gaureliano <gaabriel.aureliano@outlook.com>
2021-02-11 17:45:17 -0300

    Serialization

---
+++ 10_RESTAPI_SerializationNegotiation/RESTAPI/RESTAPI/Data/VO/PersonVO.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace RESTAPI.Data.VO
+{
+
+    public class PersonVO 
+    {
+        [JsonPropertyName("code")]
+        public long Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string FirstName { get; set;  }
+
+        [JsonPropertyName("sobrenome")]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("local")]
+        public string Address { get; set; }
+
+        [JsonPropertyName("gen")]
+        public string Gender { get; set; }
+
+    }
+}
