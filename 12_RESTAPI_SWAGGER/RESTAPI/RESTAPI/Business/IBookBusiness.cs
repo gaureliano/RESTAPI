ea53e1e406abcd9a991ea885aac9c577c6f2e5ef
gaureliano <gaabriel.aureliano@outlook.com>
2021-02-15 19:46:19 -0300

    swagger

---
+++ 12_RESTAPI_SWAGGER/RESTAPI/RESTAPI/Business/IBookBusiness.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RESTAPI.Controllers.Model;
+using RESTAPI.Data.VO;
+
+namespace RESTAPI.Business
+{
+    public interface IBookBusiness
+    {
+        BookVO Create(BookVO book);
+
+        BookVO FindByID(long id);
+
+        List<BookVO> FindAll();
+
+        BookVO Update(BookVO book);
+
+        void Delete(long id);
+    }
+}
---
+++ 12_RESTAPI_SWAGGER/RESTAPI/RESTAPI/Controllers/PersonController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RESTAPI.Controllers.Model;
+using RESTAPI.Business;
+using RESTAPI.Data.VO;
+using RESTAPI.Hypermedia.Filters;
+
+namespace RESTAPI.Controllers
+{
+    [ApiVersion("1")]
+    [ApiController]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class PersonController : ControllerBase
+    {
+        
+
+        private readonly ILogger<PersonController> _logger;
+        private IPersonBusiness _personBusiness;
+
+        public PersonController(ILogger<PersonController> logger, IPersonBusiness personBusiness)
+        {
+            _logger = logger;
+            _personBusiness = personBusiness;
+        }
+
+        [HttpGet]
+        [ProducesResponseType((200), Type = typeof(List<PersonVO>))]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get()
+        {
+            return Ok(_personBusiness.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType((200), Type = typeof(PersonVO))]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get(long id)
+        {
+            var person = _personBusiness.FindByID(id);
+            if (person == null) return NotFound();
+            return Ok( person );
+
+        }
+
+        [HttpPost]
+        [ProducesResponseType((200), Type = typeof(PersonVO))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Post([FromBody] PersonVO person )
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personBusiness.Create(person));
+
+        }
+
+        [HttpPut]
+        [ProducesResponseType((200), Type = typeof(PersonVO))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Put([FromBody] PersonVO person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personBusiness.Update(person));
+
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        public IActionResult DeletePerson(long id)
+        {
+            _personBusiness.Delete(id);
+            return NoContent();
+
+        }
+    }
+}
